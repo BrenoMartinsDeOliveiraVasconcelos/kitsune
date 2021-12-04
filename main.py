a184709783257c032f7e3db781f5a9e44ff78329
Breno Martins <crdj161@gmail.com>
2021-12-04 20:08:25 -0300

    Comandos Consegue o input do usuario + Info sobre o app

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
-from userlib import format
+from userlib import format, software
+import json
 
 
 def main():
+    # Get software info and print
+    info = json.load(open("./kitsune.json"))
+    print(f"KITSUNE PROJECT\nv{info['build']} - DEV: {info['dev']}")
+
     # Gets the current path
     path = os.getcwd()
 
@@ -11,9 +16,9 @@ def main():
         files = os.listdir(path)
         format.s_print(files)
 
-        input("> ")
-        input("> ")
-        input("> ")
+        # Gets user input, then gets the command and args
+        cmd = software.command(input("> "))
+        print(cmd)
 
 
 if __name__ == '__main__':
---
+++ userlib/software.py
@@ -0,0 +1,8 @@
+def command(string: str) -> list:
+    """
+    Return a list containing the command and its args
+    :param string: String
+    :return: list
+    """
+
+    return string.split(" ")
