f95fcb5c9f8c08dc239b77448cad4dbdccd2f16f
Breno Martins <crdj161@gmail.com>
2021-12-04 20:37:32 -0300

    Funcional É possivel navegar entre os arquivos de forma limitada e somente leitura, porém já funciona

--- main.py
+++ main.py
@@ -8,17 +8,46 @@ def main():
     info = json.load(open("./kitsune.json"))
     print(f"KITSUNE PROJECT\nv{info['build']} - DEV: {info['dev']}")
 
-    # Gets the current path
-    path = os.getcwd()
+    # Gets the current current_path
+    current_path = os.getcwd()
 
     while True:
+        # If the current path is a empty string, current path is defined to "/"
+        if current_path == "":
+            current_path = "/"
+
         # Get the files and prints
-        files = os.listdir(path)
+        files = os.listdir(current_path)
+
         format.s_print(files)
 
         # Gets user input, then gets the command and args
-        cmd = software.command(input("> "))
-        print(cmd)
+        raw_cmd = software.command(input("> "))
+
+        # Saving the commands and the args into different variables
+        cmd, arr = raw_cmd[0], raw_cmd[1:]
+
+        if cmd == "cd":
+            cd_to = current_path+"/"+"".join(arr)
+            # Checks if the current_path exists
+            if os.path.exists(cd_to):
+                # Check if it is a folder or not
+                if os.path.isdir(cd_to):
+                    # Then check if cd input is '..' or '.' or none.
+                    check_cd = (cd_to.replace("\\", "/")).split("/")
+                    if check_cd[-1] == "..":
+                        # Delete the last path
+                        del check_cd[-1]
+                        del check_cd[-1]
+                    elif check_cd[-1] == '.':
+                        del check_cd[-1]
+
+                    cd_to = "/".join(check_cd)
+                    current_path = cd_to
+                else:
+                    print("\033[31mNOT A DIRECTORY!")
+            else:
+                print("\033[31mDoes not exist.")
 
 
 if __name__ == '__main__':
