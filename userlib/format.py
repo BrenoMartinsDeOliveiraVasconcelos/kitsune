51b74d24c389eb4b72d5e7ff143d06ccb58f5fbf
Breno Martins <crdj161@gmail.com>
2021-12-19 15:46:53 -0300

    Adicionado simbolos mime

--- userlib/format.py
+++ userlib/format.py
@@ -6,7 +6,6 @@ import os
 def s_print(array: list, path: str) -> None:
     """
     Prints the content of "array" in a pretty way (or not)
-
     :param array: List
     :param path: Path
     :return: None
@@ -16,10 +15,24 @@ def s_print(array: list, path: str) -> None:
     # Prints each item of "array"
     for value in array:
         # Check the mimetype of the current value
-        mime_symbol = ""
-        mime_type = mimetypes.guess_extension(f"{path}/{value}")
+        mime_type = mimetypes.guess_type(f"{path}/{value}")[0]
+        # divide the mime type between two
+        try:
+            mime_main = mime_type.split("/")[0]
+        except (TypeError, AttributeError):
+            mime_main = ""
         if os.path.isdir(f"{path}/{value}"):
             mime_symbol = "+"
+        elif mime_main == "text":
+            mime_symbol = "w"
+        elif mime_main == "image":
+            mime_symbol = "o"
+        elif mime_main == "audio":
+            mime_symbol = ">"
+        elif mime_main == "video":
+            mime_symbol = "/"
+        elif mime_main == "application":
+            mime_symbol = "*"
         else:
             mime_symbol = "?"
         # if index is even then it will print at right of the terminal
